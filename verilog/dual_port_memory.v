44541d12833bb18552facf3eaf802ef6a516e458
Xhapa <emicb0111@gmail.com>
2023-03-29 19:53:51 -0500

    Dual port memory added

---
+++ verilog/dual_port_memory.v
@@ -0,0 +1,47 @@
+module dual_port_memory #(
+    parameter WIDTH = 128,
+    parameter HEIGHT = 64,
+    parameter BPP = 12,
+    parameter CHAINED = 1 // number of panels in chain
+) (
+    input rst,
+    input clk,
+    input wire [13:0] addr_a, addr_b, 
+    input wire [BPP-1: 0] dat_in_a, dat_in_b, 
+    input wire we_a, we_b,
+    input wire re_a, re_b,   
+    output reg [BPP-1:0] dat_out_a, dat_out_b 
+);
+
+// Dual port RAM with 8192x12 bits resolution
+reg [BPP-1:0] mem [0:CHAINED*WIDTH*HEIGHT-1];
+
+// Port A Write and Read
+always @ (posedge clk)
+begin
+    if (we_a) 
+    begin
+        mem[addr_a] <= dat_in_a;
+        dat_out_a <= dat_in_a;
+    end
+    else if (re_a)
+    begin
+        dat_out_a <= mem[addr_a];
+    end
+end
+
+// Port B Write and Read
+always @ (posedge clk)
+begin
+    if (we_b) 
+    begin
+        mem[addr_b] <= dat_in_b;
+        dat_out_b <= dat_in_b;
+    end
+    else if (re_b)
+    begin
+        dat_out_b <= mem[addr_b];
+    end
+end
+
+endmodule
--- verilog/rgb_matrix_controller.v
+++ verilog/rgb_matrix_controller.v
@@ -1,3 +1,4 @@
+`default_nettype none
 module rgb_matrix_controller #(
     parameter WIDTH = 128,
     parameter HEIGHT = 64,
@@ -9,30 +10,35 @@ module rgb_matrix_controller #(
     input wire clk, 
     input wire rst, 
 
+    // Memory signals
     input wire [13:0] addr, 
-    input wire [BPP-1: 0] rgb_indat, 
-    output wire [BPP-1: 0] rgb_outdat, // Modified to be inout for peripheral access 
+    input wire [BPP-1: 0] data_in, 
+    output reg [BPP-1: 0] data_out,
     input wire wr_en,
-    input wire rd_en, // Added input for processor read
-
+    input wire rd_en,
+    
     output reg sclk, lat, oe, a, b, c, d,
     output reg r0, g0, b0, r1, g1, b1 
 );
-// Dual port RAM with 8192x12 bits resolution
-reg [BPP-1:0] mem [0:CHAINED*WIDTH*HEIGHT-1];
 
-// Write to memory
-always @(posedge clk) begin
-    if (wr_en) begin
-        mem[addr] <= rgb_indat;
-    end
-end
+wire [13:0] addr_rgb;
+wire [BPP-1: 0] data_out_rgb, data_in_rgb, dat_out_pro;
+wire we_rgb, re_rgb;
 
-// Read to memory
-always @(posedge clk) begin
-    if (rd_en) begin
-        mem[addr] <= rgb_outdat;
-    end
-end
+dual_port_memory #(
+    .WIDTH(WIDTH), 
+    .HEIGHT(HEIGHT), 
+    .BPP(BPP), 
+    .CHAINED(CHAINED)
+    ) 
+dual_mem(
+    .rst(rst), 
+    .clk(clk), 
+    .addr_a(addr), .addr_b(addr_rgb), 
+    .dat_in_a(data_in), .dat_in_b(data_in_rgb),
+    .dat_out_a(dat_out_pro), .dat_out_b(data_out_rgb),
+    .we_a(wr_en), .we_b(we_rgb), 
+    .re_a(rd_en), .re_b(re_rgb)
+    );
 
 endmodule
